1b33d7112c2230a0d77355eb530631e18a1c11cc
mWieczorek02 <mikwie9@gmail.com>
2023-03-14 23:01:36 +0100

    finish second task

--- zad2.py
+++ zad2.py
@@ -1,34 +1,42 @@
+# Mikołaj Wieczorek s24697
+
 class Person:
-   def __init__(self, name, height, hair_color, eye_color):
+   def __init__(self, name: str, height: str, hair_color: str, eye_color: str):
        self.name = name
        self.height = height,
        self.hair_color = hair_color,
        self.eye_color = eye_color
 
-    def __str__(self):
-        return f"name: {self.name}\nheight: {self.height}\nhair color: {self.hair_color}\neye color: {self.eye_color}\n\n"
 
-people = []
+people = [
+    Person("Mikolaj", "183", "brown", "brown")
+]
 
 def print_people():
     for person in people:
-        print(f"name: {person.name}\nheight: {person.height}\nhair color: {person.hair_color}\neye color: {person.eye_color}\n\n")
+        print(f"name: {person.name}, height: {person.height}, hair color: {person.hair_color}, eye color: {person.eye_color}")
 
 def add_person():
-    name = input("input name")
-    height = input("input height")
+    name = input("input name ")
+    height = input("input height ")
+    hair_color = input("input hair color ")
+    eye_color = input("input eye color ")
+    people.append(Person(name, height, hair_color, eye_color))
 
 
 def main():
-    operation = input("choose operation: print/add")
-    match operation:
-        case "print":
-            print_people()
-        case "add":
-
-
+    flag = True
+    while flag:
+        operation = input("choose operation: print/add/stop ")
+        match operation:
+            case "print":
+                print_people()
+            case "add":
+                add_person()
+            case "stop":
+                flag = False
 
 
 
 if __name__ == "__main__":
-    print()
+    main()
